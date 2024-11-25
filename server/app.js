8a66afbc330f50244054374da4868141ecc7ce72
Vishal Mishra <vishalmishra4444@gmail.com>
2024-11-25 15:02:46 +0530

    created backend branch initial commit

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 const port = 8007;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.get("/", async (req, res) => {
   res.send("Reached backend");
